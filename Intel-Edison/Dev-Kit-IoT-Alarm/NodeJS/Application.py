3e5d9a3c5f8f4f2ee9f8d1bfa0c3bc03bb4e3da1
AdamMiltonBarker <adammiltonbarker@eu.techbubbletechnologies.com>
2017-09-17 16:13:35 +0200

    [Edison/Dev-Kit-IoT-Alarm/NodeJS] Added Project

---
+++ Intel-Edison/Dev-Kit-IoT-Alarm/NodeJS/Application.py
@@ -0,0 +1,137 @@
+# *****************************************************************************
+# Copyright (c) 2016 TechBubble Technologies and other Contributors.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html 
+#
+# Contributors:
+#   Adam Milton-Barker - TechBubble Technologies Limited
+# *****************************************************************************
+
+import time
+import sys
+import json
+
+import techbubbleiotjumpwaymqtt.application
+
+class DevKitLedApplication():
+    
+    def __init__(self):
+        
+        self.JumpWayMQTTClient = ""
+        self.configs = {}
+        
+        with open('config.json') as configs:
+            self.configs = json.loads(configs.read())
+        
+        self.startMQTT()
+            
+    def startMQTT(self):
+        
+        try:
+            
+            self.JumpWayMQTTClient = techbubbleiotjumpwaymqtt.application.JumpWayPythonMQTTApplicationConnection({
+				"locationID": self.configs["IoTJumpWaySettings"]["SystemLocation"],  
+				"applicationID": self.configs["IoTJumpWaySettings"]["SystemApplicationID"], 
+				"applicationName": self.configs["IoTJumpWaySettings"]["SystemApplicationName"], 
+				"username": self.configs["IoTJumpWayMQTTSettings"]["aUassword"], 
+				"password": self.configs["IoTJumpWayMQTTSettings"]["aPassword"]
+			})
+            
+        except Exception as e:
+            print(str(e))
+            sys.exit()
+            
+        self.JumpWayMQTTClient.connectToApplication()
+        
+DevKitLedApplication = DevKitLedApplication()
+
+while True:
+    
+    DevKitLedApplication.JumpWayMQTTClient.publishToDeviceChannel(
+		"Commands",
+		DevKitLedApplication.configs["IoTJumpWaySettings"]["SystemZone"],
+		DevKitLedApplication.configs["IoTJumpWaySettings"]["SystemDeviceID"],
+		{
+			"Actuator":"LED",
+			"ActuatorID":DevKitLedApplication.configs["Actuators"]["LED"]["ID"],
+			"Command":"TOGGLE",
+			"CommandValue":"ON"
+		}
+    )
+    
+    time.sleep(5)
+    
+    DevKitLedApplication.JumpWayMQTTClient.publishToDeviceChannel(
+		"Commands",
+		DevKitLedApplication.configs["IoTJumpWaySettings"]["SystemZone"],
+		DevKitLedApplication.configs["IoTJumpWaySettings"]["SystemDeviceID"],
+		{
+			"Actuator":"LED",
+			"ActuatorID":DevKitLedApplication.configs["Actuators"]["LED"]["ID"],
+			"Command":"TOGGLE",
+			"CommandValue":"OFF"
+		}
+    )
+    
+    time.sleep(5)
+    
+    DevKitLedApplication.JumpWayMQTTClient.publishToDeviceChannel(
+		"Commands",
+		DevKitLedApplication.configs["IoTJumpWaySettings"]["SystemZone"],
+		DevKitLedApplication.configs["IoTJumpWaySettings"]["SystemDeviceID"],
+		{
+			"Actuator":"LED",
+			"ActuatorID":DevKitLedApplication.configs["Actuators"]["LED2"]["ID"],
+			"Command":"TOGGLE",
+			"CommandValue":"ON"
+		}
+    )
+    
+    time.sleep(5)
+    
+    DevKitLedApplication.JumpWayMQTTClient.publishToDeviceChannel(
+		"Commands",
+		DevKitLedApplication.configs["IoTJumpWaySettings"]["SystemZone"],
+		DevKitLedApplication.configs["IoTJumpWaySettings"]["SystemDeviceID"],
+		{
+			"Actuator":"LED",
+			"ActuatorID":DevKitLedApplication.configs["Actuators"]["LED2"]["ID"],
+			"Command":"TOGGLE",
+			"CommandValue":"OFF"
+		}
+    )
+    
+    time.sleep(5)
+    
+    DevKitLedApplication.JumpWayMQTTClient.publishToDeviceChannel(
+		"Commands",
+		DevKitLedApplication.configs["IoTJumpWaySettings"]["SystemZone"],
+		DevKitLedApplication.configs["IoTJumpWaySettings"]["SystemDeviceID"],
+		{
+			"Actuator":"LED",
+			"ActuatorID":DevKitLedApplication.configs["Actuators"]["Buzzer"]["ID"],
+			"Command":"TOGGLE",
+			"CommandValue":"ON"
+		}
+    )
+    
+    time.sleep(5)
+    
+    DevKitLedApplication.JumpWayMQTTClient.publishToDeviceChannel(
+		"Commands",
+		DevKitLedApplication.configs["IoTJumpWaySettings"]["SystemZone"],
+		DevKitLedApplication.configs["IoTJumpWaySettings"]["SystemDeviceID"],
+		{
+			"Actuator":"LED",
+			"ActuatorID":DevKitLedApplication.configs["Actuators"]["Buzzer"]["ID"],
+			"Command":"TOGGLE",
+			"CommandValue":"OFF"
+		}
+    )
+    
+    time.sleep(5)
+    
+DevKitLedApplication.JumpWayMQTTClient.disconnectFromApplication()
---
+++ Intel-Edison/Dev-Kit-IoT-Alarm/NodeJS/index.js
@@ -0,0 +1,77 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2016 TechBubble Technologies and other Contributors.
+//
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html 
+//
+// Contributors:
+//   Adam Milton-Barker - TechBubble Technologies Limited
+////////////////////////////////////////////////////////////////////////////////
+
+var mqtt    = require('mqtt');
+var fs = require('fs');
+const mraa = require('mraa'); 
+var config = require(__dirname + '/config.json');
+var TRUSTED_CA_LIST = fs.readFileSync(__dirname + '/certs/ca.pem');
+
+var options = {
+	protocol: 'mqtts',    
+	protocolId: 'MQIsdp',
+	secureProtocol: 'TLSv1_method',
+	protocolVersion: 3,
+	certPath: TRUSTED_CA_LIST,
+	host: config.IoTJumpWayMQTTSettings.host,
+	port: config.IoTJumpWayMQTTSettings.port,
+	clientId: config.IoTJumpWaySettings.SystemDeviceName,
+	username: config.IoTJumpWayMQTTSettings.dUsername,
+	password: config.IoTJumpWayMQTTSettings.dPassword 
+};
+
+var ledOKPin = new mraa.Gpio(config.Actuators.LED.PIN); 
+var ledWarnPin = new mraa.Gpio(config.Actuators.LED2.PIN); 
+var ledAlarmPin = new mraa.Gpio(config.Actuators.Buzzer.PIN); 
+
+ledOKPin.dir(mraa.DIR_OUT); 
+ledWarnPin.dir(mraa.DIR_OUT); 
+ledAlarmPin.dir(mraa.DIR_OUT); 
+
+var client = mqtt.connect(options)
+
+client.subscribe(config.IoTJumpWaySettings.SystemLocation+'/Devices/'+config.IoTJumpWaySettings.SystemZone+'/'+config.IoTJumpWaySettings.SystemDeviceID+'/Commands')
+client.on('message', function (topic, message) {
+	data = JSON.parse(message.toString());
+	if(data.ActuatorID==131){
+		console.log("Turn On OK LED")
+		ledOKPin.write(1);
+		setTimeout(function() {
+			console.log("Turn Off OK LED")
+			ledOKPin.write(0);
+		}, 3000);
+	}
+	if(data.ActuatorID==132){
+		console.log("Turn On Warning LED")
+		ledWarnPin.write(1);
+		setTimeout(function() {
+			console.log("Turn Off Warning LED")
+			ledWarnPin.write(0);
+		}, 3000);
+	}
+	if(data.ActuatorID==133){
+		ledAlarmPin.write(1);
+		setTimeout(function() {
+			console.log("Turn Off Buzzer")
+			ledAlarmPin.write(0);
+		}, 3000);
+	}
+	
+})
+
+client.on('error', function (err) {
+  console.log(JSON.stringify(err))
+})
+
+client.on('connect', function () {
+  console.log('Connected To TechBubble IoT JumpWay')
+})
